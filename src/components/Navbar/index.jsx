1d2e38ff9b2ba3a7de47068e446d913de7a242fc
André Cristen <30880513+andrecristen@users.noreply.github.com>
2023-12-29 18:36:47 -0300

    Update index.jsx

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -90,7 +90,7 @@ const Navbar = (props) => {
                 </header>
 
                 {/* Conteúdo */}
-                <div className="px-2 mb-2 overflow-auto">
+                <div className="px-2 mb-10 overflow-auto lg:mb-2">
                     {props.children}
                 </div>
             </main>
